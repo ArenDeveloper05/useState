f2685a13b8c745915a0790a136cb2bb15ee5c9bb
ArenDeveloper05 <antisystem005@mail.ru>
2023-06-14 21:27:47 +0400

    useState usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,84 @@
-import './App.scss';
-import imageGallery from './Images';
-import { useState } from 'react';
+import "./App.scss";
+import { useState } from "react";
 
 function App() {
+  const [isHidden, setIsHidden] = useState(true);
 
-  const [src, setSrc] = useState(imageGallery[0].src);
+  const hideContent = () => {
+    setIsHidden((prev) => !prev);
+  };
 
-  const showImage = () => {
-    let number = (Math.floor(Math.random() * imageGallery.length));
-    setSrc(imageGallery[number].src);
-  }
+  const [data, setData] = useState([
+    { title: "Hi" },
+    { title: "Hello world" },
+    { title: "Good bye" },
+    { title: "hello bro" },
+  ]);
+
+  const [activeItem, setActiveItem] = useState(0);
+
+  //Form
+  const [firstNameValue, setFirstNameValue] = useState("");
+  const [lastNameValue, setLastNameValue] = useState("");
 
   return (
     <div className="App">
       <div className="container">
-        <div className="container-image">
-          <img src={src}></img>
+        <div className="container-posts">
+          {data.map((item, i) => {
+            return (
+              <div
+                className="container-posts-post"
+                key={i}
+                style={{
+                  backgroundColor: item.title.toLowerCase().includes("hello")
+                    ? "red"
+                    : "turquoise",
+                  border: i == activeItem ? "8px solid white" : "solid",
+                }}
+                onClick={() => {
+                  setActiveItem(i);
+                }}
+              >
+                {item.title}
+              </div>
+            );
+          })}
         </div>
-        <button onClick={showImage}>Show The Image</button>
+        <div className="container-form">
+          <form>
+            <input
+              type="text"
+              placeholder="First Name"
+              value={firstNameValue}
+              onChange={(e) => {
+                setFirstNameValue(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              value={lastNameValue}
+              onChange={(e) => {
+                setLastNameValue(e.target.value);
+              }}
+            />
+            <button
+              type="button"
+              onClick={() => {
+                const obj = {
+                  _firstName: firstNameValue,
+                  _lastName: lastNameValue,
+                };
+                console.log(obj);
+              }}
+            >
+              Send Form
+            </button>
+          </form>
+        </div>
+        {isHidden && <div className="container-child"></div>}
+        <button onClick={hideContent}>{!isHidden ? "Show" : "Hide"}</button>
       </div>
     </div>
   );
--- src/Images.jsx
+++ src/Images.jsx
@@ -1,25 +1,25 @@
-import img1 from './assets/images/image1.jpg';
-import img2 from './assets/images/image2.jpg';
-import img3 from './assets/images/image3.jpg';
-import img4 from './assets/images/image4.jpg';
-import img5 from './assets/images/image5.jpg';
-import img6 from './assets/images/image6.jpg';
-import img7 from './assets/images/image7.jpg';
-import img8 from './assets/images/image8.jpg';
-import img9 from './assets/images/image9.jpg';
-import img10 from './assets/images/image10.jpg';
+import img1 from "./assets/images/image1.jpg";
+import img2 from "./assets/images/image2.jpg";
+import img3 from "./assets/images/image3.jpg";
+import img4 from "./assets/images/image4.jpg";
+import img5 from "./assets/images/image5.jpg";
+import img6 from "./assets/images/image6.jpg";
+import img7 from "./assets/images/image7.jpg";
+import img8 from "./assets/images/image8.jpg";
+import img9 from "./assets/images/image9.jpg";
+import img10 from "./assets/images/image10.jpg";
 
 const imageGallery = [
-  {src:img1},
-  {src:img2},
-  {src:img3},
-  {src:img4},
-  {src:img5},
-  {src:img6},
-  {src:img7},
-  {src:img8},
-  {src:img9},
-  {src:img10}
+  { src: img1 },
+  { src: img2 },
+  { src: img3 },
+  { src: img4 },
+  { src: img5 },
+  { src: img6 },
+  { src: img7 },
+  { src: img8 },
+  { src: img9 },
+  { src: img10 },
 ];
 
 export default imageGallery;
